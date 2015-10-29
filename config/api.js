3d2c17fcb930a81788e8b9b1648a19d4d104cdac
Ginés Ortiz Sáez <gines.o.saez@gmail.com>
2015-10-30 00:00:24 +0100

    removed push.js

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 var api_config = {
     baseUrl : 'https://api.meetup.com/',
-    key : '20533ae421b571a20564d4b7c6d261c',
-    groupUrl : 'HTML5-Spain'
+    key : 'MEETUP_APIKRY',
+    groupUrl : 'MEETUP-NAME-URL'
 };
 
 module.exports = api_config;
